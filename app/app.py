5582174a275133ea28cddae453e3ebc33c3002a2
ihikky <hikkyhizzy@gmail.com>
2023-10-09 02:59:58 +0000

    Updates

--- app/app.py
+++ app/app.py
@@ -1,30 +1,35 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from elasticsearch import Elasticsearch
-import pandas as pd
-import numpy as np
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Replace with your secret key
 
 # Connect to ElasticSearch
 es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
 
 # Load the SentenceTransformers model for passage embeddings
-from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('bert-base-nli-mean-tokens')
 
 # Paths to input and output CSV files
 input_csv_path = 'docs/passage_metadata_emb.csv'
 
+# Create a Flask-WTF form for the question input
+class QuestionForm(FlaskForm):
+    question = StringField('Enter your question:')
+    submit = SubmitField('Submit')
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to the Question-Answering System API!'
-
+def index():
+    form = QuestionForm()
+    return render_template('index.html', form=form)
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
     try:
-        data = request.get_json()
+        data = request.form
         question = data['question']
 
         # Define the Elasticsearch query based on the user's question
@@ -45,11 +50,10 @@ def ask_question():
             passage = hit['_source']['Passage']
             relevant_passages.append(passage)
 
-        return jsonify({'answers': relevant_passages})
+        return render_template('results.html', question=question, answers=relevant_passages)
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
